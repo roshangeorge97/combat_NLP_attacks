a3d35db79fb8c8b7bf9d121a1c99b4ba32d371b4
al1729 <andyliu1708@gmail.com>
2023-12-03 16:31:16 -0500

    inference code for llama-7b classifier

--- bad_dataloader.py
+++ bad_dataloader.py
@@ -1,28 +1,63 @@
 import json
+import torch
+import random
+from transformers import LlamaTokenizer
+import pdb
 
-def fit_to_prompt_template(messages, labels):
-    prompt = f'Given the following list of statements, label the final statement as {labels["ok"]} or {labels["notok"]}\n'
-    for idx, message in enumerate(messages):
-        prompt += message
-        prompt += '\n'
-    prompt += 'Label: '
+class BadDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, text):
+        self.encodings = encodings
+        self.text = text
+        # self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['text'] = self.text[idx]
+        # item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        # return len(self.text)
+        return len(self.encodings.input_ids)
+
+def fit_to_prompt_template(messages, label, labels_dict):
+    message = messages[-1]
+    prompt = f'Classify the following message as either {labels_dict["ok"]} or {labels_dict["notok"]}.\nMessage: {message}\nLabel: {labels_dict[label]}'
     return(prompt)
 
-with open('/home/andyliu/Miniconda3/envs/anlp-hw/lib/python3.10/site-packages/data/bot_adversarial_dialogue/dialogue_datasets/bot_adversarial_dialogue_datasets_with_persona/train.txt', 'r') as f:
-    bad_data = f.read().split('\n')
+def load_dataset(tokenizer, data_path='/home/andyliu/Miniconda3/envs/anlp-hw/lib/python3.10/site-packages/data/bot_adversarial_dialogue/dialogue_datasets/bot_adversarial_dialogue_datasets_with_persona/train.txt', sample_rate = 0.1):
+    with open(data_path, 'r') as f:
+        bad_data = f.read().split('\n')
 
-to_json = []
-prompt_labels = {'notok':'offensive', 'ok':'inoffensive'}
-labels = {'notok':1, 'ok':0}
+    texts = []
+    prompt_labels = {'notok':'offensive', 'ok':'inoffensive'}
+    # labels = {'notok':1, 'ok':0}
+    random.seed(0) 
 
-for line in bad_data:
-    if line != '':
-        text = line.split('\t')[0].replace('text:', '').split('\\n')
-        prompt = fit_to_prompt_template(text, prompt_labels)
-        label = labels[line.split('\t')[1].replace('labels:', '').replace('_', '')]
-        to_json.append({'text':prompt, 'label':label})
+    for line in bad_data:
+        if line != '' and random.random() < sample_rate:
+            text = line.split('\t')[0].replace('text:', '').split('\\n')
+            label = line.split('\t')[1].replace('labels:', '').replace('_', '')
+            prompt = fit_to_prompt_template(text, label, prompt_labels)
+            texts.append(prompt)
 
-# to_write = json.dumps(to_json)
-with open('bad_data.json', 'w') as f: 
-    to_write = json.dumps({'train':to_json})
-    f.write(to_write)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = 'right'
+
+    shuffled_indices = list(range(len(texts)))
+    train_indices = shuffled_indices[:int(0.7*len(texts))]
+    val_indices = shuffled_indices[int(0.7*len(texts)):int(0.85*len(texts))]
+    test_indices = shuffled_indices[int(0.85*len(texts)):]
+
+    # train_dataset = BadDataset([texts[i] for i in train_indices])
+    # val_dataset = BadDataset([texts[i] for i in val_indices])
+    # test_dataset = BadDataset([texts[i] for i in test_indices])
+
+    train_encodings = tokenizer([texts[i] for i in train_indices], truncation=True, padding=True, max_length=128)
+    val_encodings = tokenizer([texts[i] for i in val_indices], truncation=True, padding=True, max_length=128)
+    test_encodings = tokenizer([texts[i] for i in test_indices], truncation=True, padding=True, max_length=128)
+    train_dataset = BadDataset(train_encodings, [texts[i] for i in train_indices])
+    val_dataset = BadDataset(val_encodings, [texts[i] for i in val_indices])
+    test_dataset = BadDataset(test_encodings, [texts[i] for i in test_indices])
+
+    return(train_dataset, val_dataset, test_dataset)
---
+++ inference.py
@@ -0,0 +1,141 @@
+from transformers import (
+    GPTQConfig, 
+    AutoModelForCausalLM, 
+    LlamaTokenizer)
+from transformers import GenerationConfig
+from peft import get_peft_config, get_peft_model, PeftModel, PeftConfig
+
+from bad_dataloader import load_dataset, fit_to_prompt_template
+from torch.utils.data import DataLoader
+import random
+
+import torch
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score
+
+from googleapiclient import discovery
+import os
+
+import argparse
+import time
+import pdb
+
+def get_model_score(model, tokenizer, text):
+    inoffensive_token = tokenizer.encode('inoffensive')[1]
+    offensive_token = tokenizer.encode('offensive')[1]
+    inputs = tokenizer(text, return_tensors="pt")['input_ids'].to(model.device)
+    generation_output = model.generate(input_ids=inputs, max_new_tokens=3, generation_config=GenerationConfig(do_sample=False), output_scores=True, return_dict_in_generate=True)
+    toxicity_logits = torch.softmax(generation_output.scores[0][:,[inoffensive_token,offensive_token]], dim=-1)
+    return(toxicity_logits[:,1].item())
+
+def get_api_score(client, text):
+    try:
+        analyze_request = {
+        'comment': { 'text': text},
+        'requestedAttributes': {'TOXICITY': {}}
+        }
+        response = client.comments().analyze(body=analyze_request).execute()
+        score = response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+        return(score)
+    except Exception as e:
+        return(0.0)
+
+def get_threshold(model, tokenizer, texts):
+    toxicity_scores = np.zeros((len(texts),))
+    for idx, text in enumerate(texts):
+        toxicity_scores[idx] = get_model_score(model, tokenizer, text)
+    return(np.quantile(toxicity_scores, 0.6))
+
+def get_labels(model, tokenizer, texts, threshold = 0.5):
+    toxicity_scores = np.zeros((len(texts),))
+    for idx, text in enumerate(texts):
+        toxicity_scores[idx] = get_model_score(model, tokenizer, text)
+    labels = (toxicity_scores > threshold).astype(int)
+    return(labels, toxicity_scores)
+
+def get_api_threshold(client, texts):
+    toxicity_scores = np.zeros((len(texts),))
+    for idx, text in enumerate(texts):
+        toxicity_scores[idx] = get_api_score(client, text)
+        time.sleep(0.5)
+    return(np.quantile(toxicity_scores, 0.6))
+
+def get_api_labels(client, texts, threshold=0.5):
+    toxicity_scores = np.zeros((len(texts),))
+    for idx, text in enumerate(texts):
+        toxicity_scores[idx] = get_api_score(client, text)
+        time.sleep(0.5)
+    labels = (toxicity_scores > threshold).astype(int)
+    return(labels, toxicity_scores)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-api', action='store_true')
+    parser.add_argument('--peft-model-path', type=str, default='/data/tir/projects/tir3/users/andyliu/Llama-2-7b-chat-hf_finetuned_andy1203full/checkpoint-24246')
+    parser.add_argument('--tokenizer-path', type=str, default='/data/datasets/models/huggingface/meta-llama/Llama-2-7b-chat-hf')
+    parser.add_argument('--test-data', type=str, default=None)
+    parser.add_argument('--threshold', type=float, default=0.415)
+    parser.add_argument('--calibrate', action='store_true')
+    parser.add_argument('--output-path', type=str, default='test.csv')
+    args = parser.parse_args()
+    return(args)
+
+if __name__ == "__main__":
+    args = parse_args()
+    tokenizer_path = args.tokenizer_path
+    tokenizer = LlamaTokenizer.from_pretrained(tokenizer_path)
+
+    if args.test_data is not None:
+        # do stuff to load dataset
+        responses = list(pd.read_csv(args.test_data)['responses'])
+        test_texts = [f'Classify the following message as either inoffensive or offensive.\nMessage: {response}\nLabel: ' for response in responses]
+
+        peft_model_path = args.peft_model_path
+        config = PeftConfig.from_pretrained(peft_model_path)
+        model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
+        model = PeftModel.from_pretrained(model, peft_model_path)
+        model = model.to('cuda')
+        model.eval()
+
+        threshold = args.threshold
+        pred_labels, scores = get_labels(model, tokenizer, test_texts, threshold)
+        pd.DataFrame.from_dict({'texts':test_texts, 'pred':pred_labels, 'scores':scores}).to_csv(args.output_path)
+    
+    else:
+        train_data, val_data, test_data = load_dataset(tokenizer)
+        val_texts = [s.split('</s>')[0] for s in tokenizer.batch_decode(val_data.encodings.input_ids)]
+        test_texts = [s.split('</s>')[0] for s in tokenizer.batch_decode(test_data.encodings.input_ids)]
+
+        label_dict = {'inoffensive':0, 'offensive':1}
+        test_labels = [label_dict[x.split(' ')[-1]] for x in test_texts]
+        test_texts = [' '.join(x.split(' ')[:-1]) for x in test_texts]
+
+        if not args.use_api:
+            peft_model_path = args.peft_model_path
+            config = PeftConfig.from_pretrained(peft_model_path)
+            model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
+            model = PeftModel.from_pretrained(model, peft_model_path)
+            model = model.to('cuda')
+            model.eval()
+
+            if args.calibrate:
+                threshold = get_threshold(model, tokenizer, val_texts)
+            else:
+                threshold = args.threshold
+            pred_labels, scores = get_labels(model, tokenizer, test_texts, threshold)
+            pd.DataFrame.from_dict({'texts':test_texts, 'pred':pred_labels, 'actual':test_labels, 'scores':scores}).to_csv(args.output_path)
+            print(f'f1 is {f1_score(test_labels, pred_labels)}')
+            print(f'threshold for model score is {threshold}')
+
+        else:
+            GC_API_KEY = os.environ['GC_API_KEY']
+            client = discovery.build("commentanalyzer", "v1alpha1", 
+                                developerKey=GC_API_KEY,
+                                discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+                                static_discovery=False,)
+            threshold = get_api_threshold(client, val_texts)
+            pred_labels, scores = get_api_labels(client, test_texts, threshold)
+            print(f'f1 is {f1_score(test_labels, pred_labels)}')
+            print(f'threshold for model score is {threshold}')
+            pd.DataFrame.from_dict({'texts':test_texts, 'pred':pred_labels, 'actual':test_labels, 'scores':scores}).to_csv(args.output_path)
