10e258ec7a892bfca0daa3f1a56cec957df5235f
Alchemist75 <zhenfan@andrew.cmu.edu>
2023-12-08 22:04:05 -0500

    latest

--- figures/toxicity-over-time.py
+++ figures/toxicity-over-time.py
@@ -7,11 +7,26 @@ mean = [0.62, 0.467, 0.449, 0.423, 0.411, 0.392]
 p90 = [0.865, 0.836, 0.829, 0.779, 0.801, 0.746]
 x = np.arange(len(p10))
 
-plt.plot(x, p10, label='p10 toxicity score')
-plt.plot(x, mean, label='mean toxicity score')
-plt.plot(x, p90, label='p90 toxicity score')
+plt.plot(x, p10, label='p10 tox. score')
+plt.plot(x, mean, label='mean tox. score')
+plt.plot(x, p90, label='p90 tox. score')
 plt.legend()
-plt.title('Repeated Debate Lowers Toxicity')
-plt.ylabel('Toxicity of Model Responses')
+plt.title('Llama-chat-unc. toxicity (toxic prompt+harmless feedback)')
+plt.ylabel('Response toxicity score')
 plt.xlabel('Rounds of Debate')
-plt.savefig('repeated_interaction.png')
+plt.savefig('repeated_interaction1.png')
+
+plt.figure()
+p10 = [0.123, 0.193, 0.200, 0.191, 0.156, 0.169]
+mean= [0.358, 0.441, 0.435, 0.426, 0.414, 0.418]
+p90 = [0.795, 0.818, 0.748, 0.771, 0.751, 0.774]
+x = np.arange(len(p10))
+
+plt.plot(x, p10, label='p10 tox. score')
+plt.plot(x, mean, label='mean tox. score')
+plt.plot(x, p90, label='p90 tox. score')
+plt.legend()
+plt.title('Llama-chat-unc. toxicity (harmless prompt+toxic feedback)')
+plt.ylabel('Response toxicity score')
+plt.xlabel('Rounds of Debate')
+plt.savefig('repeated_interaction2.png')
